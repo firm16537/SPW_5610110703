222982d1bbf33895f02fd7d83db9a70ba767ece1
firm16537 <i-am-confirm@hotmail.com>
2016-04-10 00:22:58 +0700

    #02 SpaceShip
    1.สร้างยานอวกาศ สีขาว ขึ้นมา
    2.แก้ไข ขนาดหน้าต่างและแบล็คกราวด์

--- Window.java
+++ Window.java
@@ -1,13 +1,22 @@
-import java.awt.Color;
+import java.awt.*;
 import javax.swing.JFrame;
+import java.util.ArrayList;
 
 public class Window{
 	public static void main(String[] args){
-		JFrame frame = new JFrame("SPACE");
+		JFrame frame = new JFrame("SPACE HELL");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setSize(400, 700);
+		frame.setSize(400, 650);
+		frame.getContentPane().setLayout(new BorderLayout());
 
-		frame.getContentPane().setBackground(Color.BLACK);
+		GamePanel gp = new GamePanel();
+		SpaceShip s = new SpaceShip(180, 560, 20, 20);
+
+		gp.sprites.add(s);
+		gp.updateGameUI();
+
+
+		frame.getContentPane().add(gp, BorderLayout.CENTER);
 		frame.setVisible(true);
 		
 	
